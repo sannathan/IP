f807801a11aab64d20a74a69143ccc221ac3ef2d
Nathan Barbosa <nbs3@cin.ufpe.br>
2023-08-07 08:42:36 -0300

    Chama na bota

--- aula.c
+++ aula.c
@@ -12,8 +12,11 @@ int *atribuicao(int valor, int tam){
 }
 
 int main(void){
-    int *x[5]; //x ja seria um ponteiro
-    x = atribuicao(10, 5); // \forall i:0..4 @ x[i] = 10;
+    int *x, *y; //x ja seria um ponteiro
+    x = atribuicao(10, NULL, 5); // \forall i:0..4 @ x[i] = 10;
+    y = atribuicao(10, x, 5);
+    for(int i = 0; i < 5; i++)
+        printf
     for(int i = 0; i < 5; i++)
         printf("x[%d] = %d\n", i+1, x[i]);
     return 0;
