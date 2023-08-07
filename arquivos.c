23647570acf839f891275026369cf92886e0ab57
Nathan Barbosa <nbs3@cin.ufpe.br>
2023-08-07 09:37:10 -0300

    ishi

--- arquivos.c
+++ arquivos.c
@@ -3,12 +3,13 @@
 int main(void){
     FILE *arq;
 
-    arq = fopen("teste.txt", "wt");
+    arq = fopen("teste.txt", "at");
 
     if(arq == NULL){
         printf("Deu erro manehJH");
     }
-
+    fprintf(arq, "%s\n", "Alexandre e Mota");
+    printf("Deu tudo certo");
     fclose(arq);
 
 
