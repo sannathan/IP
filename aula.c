be6980a0ff8ddf31136e32aa8c6fb9655f14742a
Nathan Barbosa <nbs3@cin.ufpe.br>
2023-08-07 09:22:33 -0300

    Agora é arquivos

--- aula.c
+++ aula.c
@@ -1,23 +1,15 @@
 #include <stdio.h>
 
-int *atribuicao(int valor, int tam){
-    int *var = (int *) malloc(tam *sizeof(int));
-    if(var == NULL){
-        printf("Problema aloc em atrib\n");
+int *inicializa(int valor, int tam){
+    int *var = (int *) malloc(tam * sizeof(int));
+    if(var == NULL)
         exit(1);
-    }
     for(int i = 0; i < tam; i++)
-        var[i] =  valor;
-        return var;
+        var[i] = valor;
+    return var;
 }
 
 int main(void){
-    int *x, *y; //x ja seria um ponteiro
-    x = atribuicao(10, NULL, 5); // \forall i:0..4 @ x[i] = 10;
-    y = atribuicao(10, x, 5);
-    for(int i = 0; i < 5; i++)
-        printf
-    for(int i = 0; i < 5; i++)
-        printf("x[%d] = %d\n", i+1, x[i]);
+
     return 0;
 }
