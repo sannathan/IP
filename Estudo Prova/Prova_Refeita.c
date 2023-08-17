3cfde70bb9fa9d2973d0ecf77a2a9b22e0f6dbb6
Your Name <nbs3@cin.ufpe.br>
2023-08-16 21:43:09 -0300

    Prova 2022.2 amassada com sucesso

---
+++ Estudo Prova/Prova_Refeita.c
@@ -0,0 +1,98 @@
+#include <stdio.h>
+
+typedef struct{
+    char nome[101];
+    char CPF[12];
+    int qtd_notas;
+    int *notas;
+    float media;
+}Aluno;
+
+Aluno *carregaAlunos(int *qtdAlunos){
+    Aluno *alunos = (Aluno *) malloc((*qtdAlunos) * sizeof(Aluno));
+
+    if(alunos == NULL){
+        printf("Erro ao alocar memória");
+        exit(1);
+    }
+
+    FILE *arq;
+    arq = fopen("alunos.txt", "r");
+
+    if(arq){
+        for(int i = 0; i < *qtdAlunos; i++){
+            fscanf(arq, "%99[^\n]", alunos[i].nome);
+            fseek(arq, sizeof(alunos[i].nome) + 1, SEEK_SET); //Posiciona o indicador de posição no começo da segunda linha
+            fscanf(arq, "%11[^\n]", alunos[i].CPF);
+            fseek(arq, sizeof(alunos[i].CPF) + 1, SEEK_CUR);
+            fscanf(arq, "%d", alunos[i].qtd_notas);
+        }
+    }
+    else{
+        printf("Erro ao abrir o arquivo");
+    }
+    return alunos;
+}
+
+float *leNotas(int *qtdNotas){
+    float *notas = (float *)malloc (sizeof(float));
+    int i = 0;
+
+    printf("Digite as notas do aluno: %d\n", i);
+    while(scanf("%f", &notas[i]) > -1){
+        notas = (float *)realloc (sizeof(float) + 1);
+        *qtdNotas++;
+        i++;
+    }
+    return notas;
+}
+
+float media(float *notas, int qtdNotas){
+    float aux = 0, media = 0;
+
+    for(int i = 0; i < qtdNotas; i++){
+        aux += notas[i];
+    }
+
+    media = aux / qtdNotas;
+    return media;
+}
+
+void ordena(Aluno *alunos, int qtdAlunos){
+    Aluno cache;
+    for(int i = 0; i < qtdAlunos; i++){
+        for(int j = 0; j < qtdAlunos - i - 1; j++){
+            if(alunos[j].media < alunos[j+1].media){
+                cache = alunos[j];
+                alunos[j] = alunos[j+1];
+                alunos[j+1] = cache;
+            }
+        }
+    }
+
+}
+void salvaAlunos(Aluno *alunos, int qtdAlunos){
+    
+
+}
+
+int main(void){
+    int qtdAlunos, qtdNotas;
+    printf("Digite a quantidade de alunos:\n");
+    scanf("%d", &qtdAlunos);
+
+    Aluno *alunos = carregaAlunos(&qtdAlunos);
+
+    for(int i = 0; i < qtdAlunos; i++){
+        alunos[i].notas = leNotas(&qtdNotas);
+        alunos[i].media = media(&alunos[i].notas, qtdNotas);
+        ordena(alunos, qtdAlunos);
+    }
+
+
+
+
+
+
+    return 0;
+}
